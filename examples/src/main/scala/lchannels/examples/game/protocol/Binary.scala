05e943fc9b783bd6c34d92eb287e1fb2813ed2a1
Alceste Scalas <alceste.scalas@imperial.ac.uk>
2016-11-08 18:38:04 +0000

    Cosmetic fixes

--- examples/src/main/scala/lchannels/examples/game/protocol/Binary.scala
+++ examples/src/main/scala/lchannels/examples/game/protocol/Binary.scala
@@ -23,6 +23,7 @@
 // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+
 /** Binary protocol classes for the multiparty game.
  *  The classes in this package have been automatically generated from the
  *  multiparty game protocol:
--- examples/src/main/scala/lchannels/examples/game/protocol/ClientC.scala
+++ examples/src/main/scala/lchannels/examples/game/protocol/ClientC.scala
@@ -23,6 +23,7 @@
 // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+
 /** Multiparty protocol classes for game player C.
  *  The classes in this package have been automatically generated from the
  *  multiparty game protocol:
